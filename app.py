a57173c9612491e520a7ad49afa071ce9c4a292d
UrbanUsling <philip_akca@hotmail.com>
2023-11-28 14:27:22 +0100

    Senaste

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': "Data Analyst",
+        'location' : 'Stockholm, Sweden',
+        'salary' : '100 000'
+    },
+    {
+        'id': 2,
+        'title': "Programmer",
+        'location' : 'Göteborg, Sweden',
+        'salary' : '200 000'
+    },
+    {
+        'id': 3,
+        'title': "Tester",
+        'location' : 'Stockholm, Sweden',
+        'salary' : '300 000'
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS, greet='Hej')
+
+@app.route("/jobs")
+def list_jobs():
+    return jsonify(JOBS)
   
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)  
