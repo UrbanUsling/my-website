c9083dd8f32ca4f246ad4ad78fc1a70e1af732fd
UrbanUsling <philip_akca@hotmail.com>
2023-12-04 11:17:17 +0100

    adding

--- database.py
+++ database.py
@@ -1,5 +1,12 @@
 from sqlalchemy import create_engine, text, MetaData, Table
-db_connection_string= "mysql+pymysql://kr4o3fjsr9tfwg7gh5sp:pscale_pw_eUwIONEJ6bsdpLhdknRBsaw5llnzkKFozPnE2bKlil0@aws.connect.psdb.cloud/webshop?charset=utf8mb4"
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+db_username = config['database']['username']
+db_password = config['database']['password']
+db_connection_string= "mysql+pymysql://"+db_username+":"+db_password+"@aws.connect.psdb.cloud/webshop?charset=utf8mb4"
 engine = create_engine(db_connection_string,
         connect_args={
         "ssl": {
