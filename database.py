c77b60dc92a6e72d50bb706820416007f513fd10
UrbanUsling <philip_akca@hotmail.com>
2023-12-04 13:41:30 +0100

    ny

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine, text, MetaData, Table
 import configparser
-
+import os
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 db_username = config['database']['username']
 db_password = config['database']['password']
-db_connection_string= "mysql+pymysql://"+db_username+":"+db_password+"@aws.connect.psdb.cloud/webshop?charset=utf8mb4"
+db_connection_string= f"mysql+pymysql://{db_username}:{db_password}@aws.connect.psdb.cloud/webshop?charset=utf8mb4"
 engine = create_engine(db_connection_string,
         connect_args={
         "ssl": {
